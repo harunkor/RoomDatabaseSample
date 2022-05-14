2b3966ca1d18e3191019b9a023ce167462f2fbcb
betulakan <betulakan5972@gmail.com>
2022-05-14 23:29:50 +0300

    'Room cannot find implementation' solved

---
+++ app/src/main/java/com/example/roomdatabasesample/SleepDatabase.kt
@@ -0,0 +1,61 @@
+package com.example.roomdatabasesample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase: RoomDatabase() {
+
+    abstract fun sleepDatabaseDao(): SleepDatabaseDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context,scope: CoroutineScope): SleepDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .addCallback(SleepDatabaseCallback(scope))
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+        private class SleepDatabaseCallback(val scope: CoroutineScope): RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+
+                INSTANCE?.let{database ->
+                    scope.launch(Dispatchers.IO){
+                        populateDatabase(database.sleepDatabaseDao())
+                    }
+                }
+            }
+        }
+
+        suspend fun populateDatabase(sleepDatabaseDao: SleepDatabaseDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            sleepDatabaseDao.clear()
+
+            var sleepNight = SleepNight()
+            sleepDatabaseDao.insert(sleepNight)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/roomdatabasesample/SleepViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.roomdatabasesample
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+
+
+class SleepViewModel(private val sleepRepository: SleepRepository): ViewModel() {
+
+    val allNights : LiveData<List<SleepNight>> = sleepRepository.allSleepNights.asLiveData()
+
+    fun insert(sleepNight: SleepNight) = viewModelScope.launch {
+        sleepRepository.insert(sleepNight)
+    }
+}
+
+class SleepViewModelFactory(private val repository: SleepRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SleepViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
