bca1903b6fecec86e9a22b6315ba48e61f09be36
harunkor <harunkor@gmail.com>
2022-05-14 18:52:11 +0300

    Initial commit

---
+++ app/src/main/java/com/harunkor/roomdatabasesample/SleepApplication.kt
@@ -0,0 +1,15 @@
+package com.harunkor.roomdatabasesample
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class SleepApplication : Application() {
+
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { SleepDatabase.getInstance(this, applicationScope) }
+
+    val repository by lazy { SleepRepository(database.sleepDatabaseDao()) }
+
+}
---
+++ app/src/main/java/com/harunkor/roomdatabasesample/SleepRepository.kt
@@ -0,0 +1,16 @@
+package com.harunkor.roomdatabasesample
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class SleepRepository(private val sleepDatabaseDao: SleepDatabaseDao) {
+
+    val allSleepNights : Flow<List<SleepNight>> = sleepDatabaseDao.getAllNights()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(sleepNight: SleepNight) {
+        sleepDatabaseDao.insert(sleepNight)
+    }
+
+}
